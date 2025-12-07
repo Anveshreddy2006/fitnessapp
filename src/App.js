ee4fc1d5a996e97468c138a2dc2f47bd4ec7dab5
anveshreddy2006 <padidamanveshreddy.com>
2025-12-07 22:19:57 +0530

    fix: update react-router-dom to version 6.30.2 and wrap App component in BrowserRouter
    - Updated react-router-dom dependency in package.json
    - Fixed return statement in App component to properly render JSX
    - Wrapped App component in BrowserRouter in index.js for routing support

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 
-import ExerciseDetail from "./pages/ExerciseDetail";
+import ExerciseDetail from "./pages/ExerciseDetail"; // or "./Pages/ExerciseDetail" if your folder is "Pages"
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -10,14 +10,16 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
-  <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/exercise/:id" element={<ExerciseDetail />} />
-    </Routes>
-    <Footer />
-  </Box>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
